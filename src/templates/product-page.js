af074e72e6d477b664e99d14c0ac76a492a87b7c
Reddemon Fox <him.sharma.98@gmail.com>
2021-02-15 16:49:22 +0530

    Initial Commit

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
@@ -8,58 +8,56 @@ import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const ProductPageTemplate = ({
-  image,
-  title,
-  heading,
-  description,
-  intro,
-  main,
-  testimonials,
-  fullImage,
-  pricing,
-}) => (
-  <div className="content">
-    <div
-      className="full-width-image-container margin-top-0"
-    >
-      <h2
-        className="has-text-weight-bold is-size-1"
-        style={{
-          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-          backgroundColor: '#f40',
-          color: 'white',
-          padding: '1rem',
-        }}
-      >
-        {title}
-      </h2>
+                                        image,
+                                        title,
+                                        heading,
+                                        description,
+                                        intro,
+                                        main,
+                                        testimonials,
+                                        fullImage,
+                                        pricing,
+                                    }) => (
+    <div className="content">
+        <div
+            className="full-width-image-container margin-top-0"
+        >
+            <h2
+                className="has-text-weight-bold is-size-1"
+                style={{
+                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                    backgroundColor: '#f40',
+                    color: 'white',
+                    padding: '1rem',
+                }}
+            >
+                {title}
+            </h2>
+        </div>
+        <section className="section section--gradient">
+
+        </section>
     </div>
-    <section className="section section--gradient">
-
-    </section>
-  </div>
-)
+);
 
 
-const ProductPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+const ProductPage = ({data}) => {
+    const {frontmatter} = data.markdownRemark;
 
-  return (
-    <Layout>
-      <ProductPageTemplate
-
-      />
-    </Layout>
-  )
-}
+    return (
+        <Layout>
+            <ProductPageTemplate/>
+        </Layout>
+    )
+};
 
 ProductPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.shape({
+            frontmatter: PropTypes.object,
+        }),
     }),
-  }),
-}
+};
 
 export default ProductPage
 
